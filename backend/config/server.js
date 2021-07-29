f8840ce328282e93a31338f5f052c383e40137de
Ike <ikescode@gmail.com>
2021-07-29 18:04:06 -0500

    update config for production

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://api.bargains.ike.dev",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '629d5781cc30b2e58050e690bc5f3c43'),
