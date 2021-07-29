82bbc3afeec2bcbf03dba9e5814ea73495503515
Ike <ikescode@gmail.com>
2021-07-29 18:04:30 -0500

    update footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -2,13 +2,13 @@ const Footer = () => {
   return (
     <div className="flex justify-between m-6">
       <p className="text-xs font-semibold text-gray-600">
-        Copyright &copy; 2021 <a href="https://radioplusinfo.com">Radio Plus, Inc.</a>
+        Copyright &copy; 2021 <a href="https://radioplusinfo.com" target="_blank">Radio Plus, Inc.</a>
       </p>
       <div className="flex gap-3 ml-4">
-        <a href="https://twitter.com/radioplusinfo" className="max-w-xs ml-4">
+        <a href="https://twitter.com/radioplusinfo" target="_blank" className="max-w-xs ml-4">
           <img src="/twitter.svg" alt="Twitter" />
         </a>
-        <a href="https://www.facebook.com/Radio-Plus-Online-Bargains-102160326497490" className="ml-3">
+        <a href="https://www.facebook.com/Radio-Plus-Online-Bargains-102160326497490" target="_blank" className="ml-3">
           <img src="/facebook.svg" alt="Facebook" />
         </a>
       </div>
