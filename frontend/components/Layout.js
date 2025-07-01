52b6c320e856bff65a389ad1e0d81e2f236f8335
Ike <ikescode@gmail.com>
2025-07-01 13:26:36 -0500

    data-config-phone

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -16,14 +16,8 @@ const Layout = ({ children, categories }) => {
         id="snipcart"
         data-api-key={process.env.NEXT_PUBLIC_SNIPCART_API_KEY}
         data-config-modal-style="side"
-      >
-        <address-fields section="top">
-          <div>
-            <snipcart-label for="phone">Phone number</snipcart-label>
-            <snipcart-input name="phone"></snipcart-input>
-          </div>
-        </address-fields>
-      </div>
+        data-config-phone-number="true"
+      ></div>
     </div>
   );
 };
